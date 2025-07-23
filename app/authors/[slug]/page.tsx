81f3681b0837f3c07f751d05c5716a4419cac348
viettv1 <viettv1@smartosc.com>
2025-07-23 09:49:28 +0700

    2

---
+++ app/authors/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import { getPostBySlug } from "@/lib/contentful";
+import {
+  ContentfulPost,
+  mapContentfulPostToDetailPost,
+} from "@/lib/helpers/contentfulHelpers";
+import PostDetail from "@/components/blog/PostDetail";
+import { notFound } from "next/navigation";
+
+interface BlogPostPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function ArthorPage(props: BlogPostPageProps) {
+  const { slug } = await props.params;
+
+  const rawPost = await getPostBySlug(slug);
+  if (!rawPost) return notFound();
+
+  const post = mapContentfulPostToDetailPost(
+    rawPost as unknown as ContentfulPost
+  );
+
+  return <PostDetail post={post} />;
+}
