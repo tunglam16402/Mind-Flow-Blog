a9d81921b5120395cd490bc097105253f3d7e86f
viettv1 <viettv1@smartosc.com>
2025-08-13 13:38:40 +0700

    comment page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import HeroSection from "@/components/home/HeroSection";
-import LatestPostSection from "@/components/home/LatestPostSection";
-
 export default function Home() {
   return (
     <div>
-      <HeroSection />
-      <div className="wrapper">
-        <LatestPostSection />
-      </div>
+      {/* <HeroSection /> */}
+      <div className="wrapper">{/* <LatestPostSection /> */}</div>
     </div>
   );
 }
